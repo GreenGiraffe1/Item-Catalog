3f36cd56737bc4bc3232e72b27b311ffd62a36a4
Matt Hedlund <matthewrhedlund@gmail.com>
2017-10-26 09:03:42 -0400

    modify parameter formatting

--- catalog_main.py
+++ catalog_main.py
@@ -452,7 +452,7 @@ def disconnect():
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
     app.debug = True
-    app.run(host = '0.0.0.0', port = 8000)
+    app.run(host='0.0.0.0', port=8000)
 
 
 
