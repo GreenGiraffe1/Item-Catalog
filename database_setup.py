27fde2bf752da310321acf3d0715e49ae8fa42e0
Matt Hedlund <matthewrhedlund@gmail.com>
2017-10-30 14:58:08 -0400

    Add docstrings, and unify spacing

--- database_setup.py
+++ database_setup.py
@@ -3,12 +3,14 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 
+
 Base = declarative_base()
 
 
 class User(Base):
-    __tablename__ = 'user'
+    """Object holding information about individual registered users"""
 
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
     email = Column(String(250), nullable=False)
@@ -16,7 +18,8 @@ class User(Base):
 
     @property
     def serialize(self):
-        """return object data in serializable format"""
+        """return User object data in serializable format"""
+
         return {
             'name'      :   self.name,
             'email'     :   self.email,
@@ -24,15 +27,18 @@ class User(Base):
             'id'        :   self.id,
         }
 
-class Catagory(Base):
-    __tablename__ = 'catagory'
 
+class Catagory(Base):
+    """Object holding information about item categories"""
+
+    __tablename__ = 'catagory'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
 
     @property
     def serialize(self):
-        """return object data in serializable format"""
+        """return Category object data in serializable format"""
+        
         return {
             'name'  :   self.name,
             'id'    :   self.id,
@@ -40,8 +46,9 @@ class Catagory(Base):
 
 
 class Item(Base):
-    __tablename__ = 'item'
+    """Object holding information about individual catalog items"""
 
+    __tablename__ = 'item'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
     description = Column(String(5000), nullable=False)
@@ -52,7 +59,7 @@ class Item(Base):
 
     @property
     def serialize(self):
-        """return object data in serializable format"""
+        """return Item object data in serializable format"""
         return {
             'name'          : self.name,
             'description'   : self.description,
@@ -60,10 +67,5 @@ class Item(Base):
         }
 
 
-
-
-
 engine = create_engine('sqlite:///itemcatalog.db')
-
-
 Base.metadata.create_all(engine)
