72722cb262bda8ae046b67b3a6dcd5d455a0ecc2
Matt Hedlund <matthewrhedlund@gmail.com>
2017-10-30 15:06:04 -0400

    Add module docstring

--- catalog_main.py
+++ catalog_main.py
@@ -1,3 +1,8 @@
+"""
+Catalog Web App is a project that displays a catalog of items, and allows users to interact with them. Component items are organized into one of several categories. The items can be displayed as one long comprehensive list, or grouped into sub-lists by category. An item's detailed description can be viewed by clicking on an item. Catalog Web App supports user registration, and logging via Facebook Login or Google Login OAuth2.0 verification. User's who are signed-in are able to create new items, and edit or delete items they've created. Permissions are in place to prevent users from modifying other users' content, and to prevent non-registered users from doing the same, or from creating new content themselves.
+"""
+
+
 import httplib2
 import json
 import requests
