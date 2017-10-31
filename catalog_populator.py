454fb89893b26271bd42e12648ca72cf4a56cf7a
Matt Hedlund <matthewrhedlund@gmail.com>
2017-10-31 16:30:02 -0400

    unify formatting

--- catalog_populator.py
+++ catalog_populator.py
@@ -1,44 +1,38 @@
+"""
+The purpose of this module is to load test data into the database.
+"""
+
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from database_setup import Category, Base, Item, User
 
 engine = create_engine('sqlite:///itemcatalog.db')
-# Bind the engine to the metadata of the Base class so that the
-# declaratives can be accessed through a DBSession instance
 Base.metadata.bind = engine
-
 DBSession = sessionmaker(bind=engine)
-# A DBSession() instance establishes all conversations with the database
-# and represents a "staging zone" for all the objects loaded into the
-# database session object. Any change made against the objects in the
-# session won't be persisted into the database until you call
-# session.commit(). If you're not happy about the changes, you can
-# revert all of them back to the last commit by calling
-# session.rollback()
 session = DBSession()
 
 
-# Create dummy user
-user1 = User(name="Matt-Bott",
-             email="heddy@ahed.com")
-            #  picture='https://pbs.twimg.com/profile_images/2671170543/18debd694829ed78203a5a36dd364160_400x400.png')
+user1 = User(name="Matt-Bott", email="heddy@ahed.com")
 session.add(user1)
 session.commit()
 
-# Menu for UrbanBurger
+
 category1 = Category(name="Computer Processors")
 session.add(category1)
 session.commit()
 
-item1 = Item(user_id=1, name="Intel i7-7700K", description="4 cores, 8 threads running at base-clock of 4.2GHz",
-             category=category1)
 
+item1 = Item(user_id=1, name="Intel i7-7700K",
+             description="4 cores, 8 threads running at base-clock of 4.2GHz",
+             category=category1)
 session.add(item1)
 session.commit()
 
 
-item2 = Item(user_id=1, name="Intel i5-7600K", description="4 cores, 4 threads running at base-clock of 3.8GHz",
+item2 = Item(user_id=1, name="Intel i5-7600K",
+             description="4 cores, 4 threads running at base-clock of 3.8GHz",
              category=category1)
 session.add(item2)
 session.commit()
@@ -49,29 +43,28 @@ session.add(category2)
 session.commit()
 
 
-
-
-
 item3 = Item(user_id=1, name="GTX 1080", description="PUT SOMETHING HERE",
              category=category2)
 session.add(item3)
 session.commit()
+
 
 item4 = Item(user_id=1, name="RX 580 8GB", description="PUT SOMETHING HERE",
              category=category2)
 session.add(item4)
 session.commit()
 
+
 category3 = Category(name="Computer Monitors")
 session.add(category3)
 session.commit()
 
 
-item5 = Item(user_id=1, name="ASUS 23.8 inch ", description="PUT SOMETHING HERE",
+item5 = Item(user_id=1, name="ASUS 23.8 inch ",
+             description="PUT SOMETHING HERE",
              category=category3)
 session.add(item5)
 session.commit()
-
 
 
 item6 = Item(user_id=1, name="HP OMEN 25", description="PUT SOMETHING HERE",
@@ -80,16 +73,4 @@ session.add(item6)
 session.commit()
 
 
-
-
-
-
-
-
-
-
-
-
 print "Added Items & Categories to App!"
-
-# END
