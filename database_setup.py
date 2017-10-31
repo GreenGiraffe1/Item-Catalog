179edbb0096a7d111b6764ecfd7865543472de61
Matt Hedlund <matthewrhedlund@gmail.com>
2017-10-31 16:33:50 -0400

    Add module docstring

--- database_setup.py
+++ database_setup.py
@@ -1,3 +1,8 @@
+"""
+Setup database.
+"""
+
+
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
